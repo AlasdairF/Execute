34cdf5c69335768e8716e0258f15224c231b21ec
AlasdairF <AlasdairF@users.noreply.github.com>
2015-07-23 12:32:32 +0700

    Created

---
+++ execute.go
@@ -0,0 +1,35 @@
+go execute
+
+import (
+ "time"
+ "errors"
+ "os/exec"
+)
+
+func Timeout(duration int64, name string, arg ...string) error {
+	
+	cmd := exec.Command(name, ...arg)
+	err := cmd.Start()
+	if err != nil {
+		return err
+	}
+	
+	done := make(chan error, 1)
+	go func() {
+		done <- cmd.Wait()
+	}()
+	select {
+		case <- time.After(duration):
+			if err = cmd.Process.Kill(); err != nil {
+				return err
+			}
+			<- done // allow goroutine to exit
+			return errors.New(`Timeout`)
+		case err = <- done:
+			if err != nil {
+				return err
+			}
+	}
+	return nil
+	
+}
