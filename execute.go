fe4d20201a73a3e3fdccebf632dcdcf03c7ec64e
AlasdairF <AlasdairF@users.noreply.github.com>
2016-09-14 17:22:02 +0700

    Added Output()

--- execute.go
+++ execute.go
@@ -98,3 +98,44 @@ func (c *cmd) CombinedOutput() ([]byte, error) {
 	return b.BytesCopy(), nil
 	
 }
+
+func (c *cmd) Output() ([]byte, error) {
+	
+	cmd := exec.Command(c.name, c.arg...)
+	
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		return nil, err
+	}
+	defer stdout.Close()
+	
+	b := custom.NewBuffer(0)
+	defer b.Close()
+	
+	err = cmd.Start()
+	if err != nil {
+		return nil, err
+	}
+	
+	go io.Copy(b, stdout)
+	
+	done := make(chan error, 1)
+	go func() {
+		done <- cmd.Wait()
+	}()
+	select {
+		case <- time.After(c.duration):
+			if err = cmd.Process.Kill(); err != nil {
+				return nil, err
+			}
+			<- done // allow goroutine to exit
+			return nil, ErrTimeout
+		case err = <- done:
+			if err != nil {
+				return nil, err
+			}
+	}
+	
+	return b.BytesCopy(), nil
+	
+}
